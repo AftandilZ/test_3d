521e158725602e47c618aeeab5686dce92d636ff
AftandilZ <121285906+AftandilZ@users.noreply.github.com>
2023-02-14 18:18:24 +0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/c7KHf8m4g-mccEa3/scene.splinecode');
